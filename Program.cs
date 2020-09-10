c12dc11b1f612e89a8125fc7c0c9adecb9976e07
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2020-09-11 00:00:29 +0200

    Add initial project setup

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MotiveSitemapper
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "Hello, World!" );
+
+            if ( args.Length < 1 )
+            {
+                Console.Error.WriteLine( "" );
+            }
+        }
+    }
+}
